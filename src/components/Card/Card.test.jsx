878de4498cc729d805988ce040ef7ff0e9dbd12f
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-03-28 01:34:49 -0300

    feat - test and completing Card component

--- src/components/Card/Card.test.jsx
+++ src/components/Card/Card.test.jsx
@@ -1,6 +1,9 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/test/helper'
 import Card from './index'
+import BgDarkBlue from 'assets/images/bg-blue_card.svg'
+import BgBlue from 'assets/images/bg-blue_light.svg'
+import theme from 'styles/theme'
 
 const props = {
   date: "02/06",
@@ -14,12 +17,29 @@ const props = {
 }
 
 describe('<Card />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(
-      <Card {...props} />)
+  it('should render the component', () => {
+    const { container } = renderWithTheme(<Card {...props} />)
 
     expect(screen.getByRole('heading', { name: /Lista 03 - Fisica Moderna - Modulo 1 - Modulo 2 - Aula 4/i })).toBeInTheDocument()
+    expect(screen.getByText('Até 02/06 8h30')).toBeInTheDocument()
+    expect(screen.getByText('Matemática')).toBeInTheDocument()
+    expect(screen.getByText('Ricardo')).toBeInTheDocument()
+    expect(screen.getByText('0/100')).toBeInTheDocument()
 
+    expect(container.firstChild).toMatchSnapshot()
+  })
 
+  it('should render the bgBlueDark', () => {
+    const { container } = renderWithTheme(<Card {...props} type="1" />)
+    expect(container.firstChild).toHaveStyle({
+      background: `url(${BgDarkBlue}) no-repeat center center / cover, ${theme.colors['darkBlue']}`,
+    })
+  })
+
+  it('should render the bgBlue', () => {
+    const { container } = renderWithTheme(<Card {...props} type="0" />)
+    expect(container.firstChild).toHaveStyle({
+      background: `url(${BgBlue}) no-repeat center center / cover, ${theme.colors['blue']}`,
+    })
   })
 })
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,8 +1,47 @@
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
-  Content: {
-    background: ({ theme, type, background }) => `url(${background}) no-repeat center center / cover, ${type === 1 ? theme.colors['darkBlue'] : theme.colors['blue']}`
+  Content: ({ background, theme, type }) => ({
+    background: `url(${background}) no-repeat center center / cover, ${type === 1 ? theme.colors['darkBlue'] : theme.colors['blue']}`,
+    '& span': {
+      display: 'block',
+      fontSize: `1.4rem`,
+      color: `${theme.colors.white}`
+    },
+    borderRadius: theme.border.radius,
+    padding: theme.spacings.xsmall
+  }),
+  HeaderCard: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  Hour: ({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    background: theme.colors.red,
+    padding: '0.2rem 0.4rem 0.2rem 0.4rem',
+    borderRadius: theme.border.radius_two,
+
+    '& span': {
+      marginLeft: '0.8rem'
+    }
+  }),
+  Title: ({ theme }) => ({
+    fontSize: theme.font.sizes.small,
+    color: theme.colors.white,
+    lineHeight: '1.9rem',
+    maxWidth: '32rem',
+    margin: `${theme.spacings.small} 0`,
+    fontWeight: theme.font.bold
+  }),
+  FooterCard: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  Class: {
+
   }
 })
 
