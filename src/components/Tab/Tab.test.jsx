a53d8cbc264cd70494f5ea24cb6868e8c8f9b0ec
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-03-28 19:11:09 -0300

    test - test Tab component

--- src/components/Tab/Tab.test.jsx
+++ src/components/Tab/Tab.test.jsx
@@ -3,9 +3,18 @@ import { renderWithTheme } from 'utils/test/helper'
 import Tab from './index'
 
 describe('<Tab />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<Tab label="teste" />)
-
-
+  it('should render the label', () => {
+    renderWithTheme(<Tab label="teste" />)
+    expect(screen.getByText('teste')).toBeInTheDocument()
+    expect(screen.getByRole('listitem')).toHaveStyle({
+      color: '#414042',
+    })
+  })
+  it('should active link', () => {
+    renderWithTheme(<Tab label="teste" active />)
+    expect(screen.getByRole('listitem')).toHaveStyle({
+      color: '#339CF7',
+      'border-bottom': `2px solid #339CF7`,
+    })
   })
 })
